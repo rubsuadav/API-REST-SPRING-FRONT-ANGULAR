cab3faf49d60b3a385e3ebc84599ab457a4880ed
rubsuadav <rubsuadav@alum.us.es>
2025-02-10 20:08:35 +0100

    add CORS configuration to SecurityConfig for improved API access control

--- backend/src/main/java/com/app/prueba/config/SecurityConfig.java
+++ backend/src/main/java/com/app/prueba/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.app.prueba.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,17 +28,23 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((requests) -> requests
-                // Swagger
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui.html").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests((requests) -> requests
+                        // Swagger
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
 
-                // API
-                .requestMatchers(HttpMethod.GET, "/", "/api/users", "/api/users/**", "/api/cards/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/", "/api/users", "/api/users/**", "/api/auth/**", "/api/cards/**").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/", "/api/users", "/api/users/**", "/api/cards/**").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/", "/api/users", "/api/users/**", "/api/cards/**").permitAll())
+                        // API
+                        .requestMatchers(HttpMethod.GET, "/", "/api/users", "/api/users/**", "/api/cards/**")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.POST, "/", "/api/users", "/api/users/**", "/api/auth/**",
+                                "/api/cards/**")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/", "/api/users", "/api/users/**", "/api/cards/**")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/", "/api/users", "/api/users/**", "/api/cards/**")
+                        .permitAll())
 
                 // GUARDS
                 .addFilterBefore(anonymousFilter, UsernamePasswordAuthenticationFilter.class)
@@ -45,5 +56,18 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setMaxAge(3600L);
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
